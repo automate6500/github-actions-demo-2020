e347af72c19e2a59372ee2033bcb9239ebff0733
Michael <michael@managedkaos.com>
2019-01-24 01:52:15 -0800

    Use --quiet and --upgrade on all pips

--- .github/action-lint/entrypoint.sh
+++ .github/action-lint/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo ">>>>>>>> Update pip and install requirements"
-pip install -U pip
+pip install --quiet --upgrade pip
 pip install --quiet --requirement requirements.txt
 
 echo ">>>>>>>> Run flake8"
