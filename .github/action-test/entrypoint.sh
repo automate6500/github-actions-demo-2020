76aee992d892533b79dc82f2aa99ecd2b9196a62
Michael <michael@managedkaos.com>
2019-01-24 01:39:24 -0800

    Add action 'Test'

---
+++ .github/action-test/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Update pip and install requirements"
+pip install -U pip
+pip install --quiet --requirement requirements.txt
+
+echo "Run pytest"
+pytest --verbose --junitxml=junit/test-results.xml
