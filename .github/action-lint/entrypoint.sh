d25b35ba8b75cd669ac56d288d3bffcace8f6625
Michael <michael@managedkaos.com>
2019-01-24 01:18:10 -0800

    Add some echoes

--- .github/action-lint/entrypoint.sh
+++ .github/action-lint/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-pip install --update pip
+
+echo "Update pip and install requirements"
+pip install -U pip
 pip install --requirement requirements.txt
+
+echo "Run flake8"
 flake8 --ignore=E501,E231
+
+echo "Run pylint"
 pylint --errors-only --disable=C0301 --disable=C0326 *.py tests/*.py
