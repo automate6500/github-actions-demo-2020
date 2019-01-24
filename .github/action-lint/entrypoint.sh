4ee82a2bd75466e9888eeab170f6bba176e29103
Michael <michael@managedkaos.com>
2019-01-24 02:00:32 -0800

    OK, with the -xe the echoes aren't really needed....

--- .github/action-lint/entrypoint.sh
+++ .github/action-lint/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -xe
-
-echo ">>>>>>>> Update pip and install requirements"
 pip install --quiet --upgrade pip
 pip install --quiet --requirement requirements.txt
-
-echo ">>>>>>>> Run flake8"
 flake8 --ignore=E501,E231
-
-echo ">>>>>>>> Run pylint"
 pylint --disable=C0301 --disable=C0326 *.py tests/*.py
--- .github/action-test/entrypoint.sh
+++ .github/action-test/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -xe
-
-echo ">>>>>>>> Update pip and install requirements"
 pip install --quiet --upgrade pip
 pip install --quiet --requirement requirements.txt
-
-echo ">>>>>>>> Run pytest"
 python -m pytest --verbose --junitxml=junit/test-results.xml
